2e92f31ac9417d3d093c79f029fe364d74baaf91
Emma 256 <mremmaug@gmail.com>
2024-10-22 22:29:32 -0700

    Add files via upload

---
+++ Question1.js
@@ -0,0 +1,4 @@
+/*QN.Define and differentiate between variables and constants in JavaScript.*/
+
+//Variables are containers for storing data values that can be reassigned while
+//Constants are variables that cannot be reassigned once they have been declared.
