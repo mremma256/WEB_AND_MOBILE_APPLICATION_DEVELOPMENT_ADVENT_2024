96fcf5402ff7d0f875c234b0e14dc141a11c5e5b
Emma 256 <mremmaug@gmail.com>
2024-10-27 23:16:36 -0700

    Add files via upload

---
+++ Question3.js
@@ -0,0 +1,11 @@
+/*QN.Explain the difference between var, let, and const in JavaScript.*/
+
+// var is a variable that can be reassigned.
+
+// let is a block of variables that cannot be reassigned.
+
+// const is a variable that cannot be reassigned.
+
+/*QN.What happens if you try to reassign values to variables declared with let or const?*/
+
+// Reassigning values declared with let will run while those with const will return an error. 
