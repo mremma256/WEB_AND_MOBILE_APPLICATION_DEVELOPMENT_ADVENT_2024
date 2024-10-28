721b423109f034ed517c5990b337c56e385f520e
Emma 256 <mremmaug@gmail.com>
2024-10-27 23:14:13 -0700

    Add files via upload

---
+++ Question2.js
@@ -0,0 +1,5 @@
+/*QN. Write a JavaScript program that stores the name of a country in Africa and its population
+using variables, and the current year as a constant*/
+var country = "Uganda";
+let population = 50000000;
+const currentYear = 2024;
