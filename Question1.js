0838ec8459df587a3dca5866598f514b4627192e
Emma 256 <mremmaug@gmail.com>
2024-11-01 22:37:31 +0300

    Add files via upload

--- Question1.js
+++ Question1.js
@@ -1,8 +1,9 @@
-/*Write a function in JavaScript called calculateTax that accepts the income of a
-person in Nigeria and returns the tax payable (assume a tax rate of 15%).*/
+/*Using a for or while loop, write a program that prints the rainfall recorded in
+millimeters for each of the last 10 days in a rural region in Ghana. Assume the rainfall
+data is stored in a list.*/
 
-function calculateTax(income) {//start of the function
-    let tax = income * 0.15;//Tax calculation
-    return tax;
+let rainfallData = [100, 120, 115, 98, 130, 105, 112, 125, 108, 110];//list of rainfall records
+
+for (let i = 0; i < rainfallData.length; i++) {
+    console.log(`Day ${i + 1}: ${rainfallData[i]} mm`);//Output of the rainfall records
 }
-console.log("Tax Payable:",calculateTax(50000));//Tax Output
